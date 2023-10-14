677b841786b264414a07bdedc0573d65662affdd
Виктория Князькова <vknyazkova01@gmail.com>
2023-10-14 21:21:36 +0300

    add funcs and classes to fill database

--- mathematicon/backend/models/custom_dataclasses.py
+++ mathematicon/backend/models/custom_dataclasses.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from typing import List
+from spacy.tokens import Span, Token, Doc
 
 
 @dataclass
@@ -31,3 +32,103 @@ class QueryInfo:
     pos_string: str = ''
     lemmatized: str = ''
     formula: List[str] = None
+
+
+class DatabaseToken:
+    def __init__(self,
+                 spacy_token: Token,
+                 pos_in_text: int,
+                 char_start: int,
+                 char_end: int,
+                 filename: str,
+                 sent_pos_in_text: int):
+        self.token = spacy_token.text
+        self.whitespace = 1 if spacy_token.whitespace_ else 0
+        self.lemma = spacy_token.lemma_
+        self.pos = spacy_token.pos_
+
+        self.pos_in_text = pos_in_text
+        self.char_start = char_start
+        self.char_end = char_end
+        self.filename = filename
+        self.sent_pos_in_text = sent_pos_in_text
+
+
+class DatabaseText:
+    def __init__(self,
+                 sentences: Doc,
+                 filename: str,
+                 title: str,
+                 yb_link: str,
+                 branch: str,
+                 level: str):
+        self._sentences = sentences
+
+        self.filename = filename
+        self.title = title
+        self.yb_link = yb_link
+        self.branch = branch
+        self.level = level
+
+    def __iter__(self):
+        for i, sent in enumerate(self._sentences.sents, start=1):
+            sentence = DatabaseSentence(
+                spacy_span=sent,
+                pos_in_text=i,
+                filename=self.filename
+            )
+            yield sentence
+
+    def dict_(self):
+        return {k: v for k, v in vars(self).items() if not k.startswith('_')}
+
+
+class DatabaseSentence:
+    def __init__(self,
+                 spacy_span: Span,
+                 pos_in_text: int,
+                 filename: str):
+        self._sent = spacy_span
+
+        self.sent_text = spacy_span.text
+        self.lemmatized = ' '.join((t.lemma_ for t in spacy_span))
+        self.pos_in_text = pos_in_text
+        self.filename = filename
+
+    def tokens_attr(self, attr_name: str, out_style: str = 'tuple'):
+        """
+        Yields attribute value of attr_name for every token in the sentence
+        Args:
+            attr_name: attribute name to retrieve value
+            out_style: how to return value
+                'tuple': returns tuple (attr_val,)  (useful for qmark placeholder style)
+                'val': returns value only
+                'dict': returns dict {attr_name: attr_val} (useful for named placeholders)
+
+        Returns: attribute value in out_style
+
+        """
+        for t in self._sent:
+            attr_val = getattr(t, attr_name)
+            if out_style == 'tuple':
+                yield (attr_val,)
+            elif out_style == 'val':
+                yield attr_val
+            elif out_style == 'dict':
+                yield {attr_name, attr_val}
+
+    def token_info_generator(self):
+        char_cur = 0
+        for i, t in enumerate(self._sent, start=1):
+            char_end = char_cur + len(t.text)
+            token = DatabaseToken(spacy_token=t,
+                                  pos_in_text=i,
+                                  char_start=char_cur,
+                                  char_end=char_end + 1,
+                                  filename=self.filename,
+                                  sent_pos_in_text=self.pos_in_text)
+            yield token
+            char_cur = char_end + len(t.whitespace_)
+
+    def dict_(self):
+        return {k: v for k, v in vars(self).items() if not k.startswith('_')}
--- mathematicon/backend/models/database.py
+++ mathematicon/backend/models/database.py
@@ -1,5 +1,7 @@
 import sqlite3
-from typing import Iterable
+from typing import Iterable, Tuple
+
+from .custom_dataclasses import DatabaseToken, DatabaseSentence, DatabaseText
 
 
 class DBHandler:
@@ -36,179 +38,128 @@ class UserDBHandler(DBHandler):
 
 
 class TextDBHandler(DBHandler):
-    def add_math_branch(self, name: str) -> int:
-        with self.conn:
-            cur = self.conn.execute('''
-            INSERT or IGNORE INTO math_branches (name)
-            VALUES (?)
-            RETURNING id''', (name,))
-            added_id = cur.fetchone()
 
-            if not added_id:
-                self.conn.execute('''
-                SELECT id
-                FROM math_branches
-                WHERE name = (?)''', (name,))
-                added_id = cur.fetchone()
-        return added_id[0]
+    def add_math_branch(self, name: str, commit: bool = True):
+        self.conn.execute('''
+        INSERT or IGNORE INTO math_brances (name)
+        VALUES (?)''', (name, ))
+        if commit:
+            self.conn.commit()
 
-    def add_text_level(self, name) -> int:
-        with self.conn:
-            cur = self.conn.execute('''
-                        INSERT or IGNORE INTO text_difficulty (name)
-                        VALUES (?)
-                        RETURNING id''', (name,))
-            added_id = cur.fetchone()
+    def add_text_level(self, name, commit: bool = True):
+        self.conn.execute('''
+                    INSERT or IGNORE INTO text_difficulty (name)
+                    VALUES (?)''', (name,))
+        if commit:
+            self.conn.commit()
 
-            if not added_id:
-                self.conn.execute('''
-                            SELECT id
-                            FROM text_difficulty
-                            WHERE name = (?)''', (name,))
-                added_id = cur.fetchone()
-        return added_id[0]
-
-    def add_text(self, text_info: dict) -> int:
+    def add_text(self, text: DatabaseText):
         """
-        Add record about text to database
+        Adds record about text to database or updates if filename exists
         Args:
-            text_info: dict with keys
-                title: text title
-                branch: math branch
-                level: difficulty level
-                filename: name where this text is stored
-                yb_link: link to youtube video
-
-        Returns: inserted text id
-
+            text_info: instance of DatabaseText class
         """
         with self.conn:
-            text_info['branch'] = self.add_math_branch(text_info['branch'])
-            text_info['level'] = self.add_text_level(text_info['level'])
+            text_info = text.dict_()
+            self.add_math_branch(text_info['branch'], commit=False)
+            self.add_text_level(text_info['level'], commit=False)
 
-            cur = self.conn.execute('''
-            INSERT or IGNORE INTO texts (title, filename, youtube_link, level_id, math_branch_id)
-            VALUES (:title, :filename, :yb_link, :level, :branch)
-            RETURNING id''', text_info)
-            added_id = cur.fetchone()[0]
+            self.conn.execute('''
+            INSERT INTO texts (title, filename, youtube_link, level_id, math_branch_id)
+            VALUES (
+            :title, 
+            :filename, 
+            :yb_link, 
+            (SELECT id FROM text_difficulty WHERE name = :level), 
+            (SELECT id FROM math_branches WHERE name = :branch)
+            )
+            ON CONFLICT (filename)
+            DO UPDATE SET
+            title = :title,
+            youtube_link = : yb_link,
+            level_id = (SELECT id FROM text_difficulty WHERE name = :level),
+            branch = (SELECT id FROM math_branches WHERE name = :branch)
+            WHERE filename = :filename''', text_info)
 
-            if not added_id:
-                cur = self.conn.execute('''
-                            SELECT id
-                            FROM texts
-                            WHERE filename = :filename''', text_info)
-                added_id = cur.fetchone()[0]
-        return added_id
-
-    def get_text_id(self, filename: str) -> int:
-        cur = self.conn.execute('''
-        SELECT id
-        FROM texts
-        WHERE filename = ?''', (filename,))
-
-        return cur.fetchone()[0]
-
-    def add_sentence(self, sentence_info: dict) -> int:
+    def add_sentence(self, sentence: DatabaseSentence) -> int:
         """
         Adds information about sentence
         Args:
-            sentence_info: dictionary, that contains following keys:
-                text_id: text_id of the sentence
-                sent: sentence itself
-                lemmatized: lemmatized sentence
-                pos_in_text: sequence number of sentence in the text
-        Returns: sentence id
-
-        """
-        with self.conn:
-            cur = self.conn.execute('''
-            INSERT INTO sents (text_id, sent, lemmatized, pos_in_text)
-            VALUES (:text_id, :sent, :lemmatized, :pos_in_text)
-            RETURNING id''', sentence_info)
-
-        return cur.fetchone()[0]
-
-    def add_lemma(self, lemma, commit: bool = True):
-        cur = self.conn.execute('''
-        INSERT or IGNORE INTO lemmas (name)
-        VALUES (?)
-        RETURNING id''', (lemma,))
-        added_id = cur.fetchone()
-
-        if not added_id:
-            self.conn.execute('''
-            SELECT id
-            FROM lemmas
-            WHERE name = (?)''', (lemma,))
-            added_id = cur.fetchone()
-        if commit:
-            self.conn.commit()
-        return added_id[0]
-
-    def add_pos(self, pos, commit: bool = True):
-        cur = self.conn.execute('''
-                INSERT or IGNORE INTO pos (name)
-                VALUES (?)
-                RETURNING id''', (pos,))
-        added_id = cur.fetchone()
-
-        if not added_id:
-            self.conn.execute('''
-                    SELECT id
-                    FROM lemmas
-                    WHERE name = (?)''', (pos,))
-            added_id = cur.fetchone()
-        if commit:
-            self.conn.commit()
-        return added_id[0]
-
-    def add_token(self, token_info):
-        """
-        Add grammar annotation for token
-        Args:
-            token_info: dictionary
-                token: token itself
-                whitespace: 1 if there is whitespace after and 0 otherwise
-                pos_in_text: sequence number of token in text
-                char_start:
-                char_end:
-                pos: pos tag
-                lemma: token lemma
-                sent_id: id of the sentence
-
+            sentence_info: instance of DatabaseSentence class
         Returns:
 
         """
+        sentence_info = sentence.dict_()
         with self.conn:
-            token_info['pos'] = self.add_pos(token_info['pos'], commit=False)
-            token_info['lemma'] = self.add_lemma(token_info['lemma'], commit=False)
-            self.conn.execute('''
-            INSERT INTO tokens (sent_id, token, whitespace, lemma_id, pos_id, pos_in_text, char_start, char_end)
-            VALUES (:sent_id, :token, :whitespace, :lemma, :pos, :pos_in_text, :char_start, :char_end)''', token_info)
-
-    def get_sent_id(self, text_id: int, pos_in_text: int):
-        cur = self.conn.execute('''
-        SELECT id
-        FROM sents
-        WHERE text_id = (?) AND pos_in_text = (?)''', (text_id, pos_in_text))
+            cur = self.conn.execute('''
+            INSERT INTO sents (text_id, sent, lemmatized, pos_in_text)
+            VALUES (
+            (SELECT id FROM texts WHERE filename = :filename), 
+            :sent_text, 
+            :lemmatized, 
+            :pos_in_text)''', sentence_info)
         return cur.fetchone()[0]
 
-    # def update_text_grammar_info(self, filename: str, new_grammar: Iterable[Iterable[dict]]):
-    #     text_id = self.get_text_id(filename)
-    #     with self.conn:
-    #         for i, sent in new_grammar:
-    #             sent_id = self.get_sent_id(text_id, i)
-    #             for token_info in sent:
-    #                 token_info['sent_id'] = sent_id
-    #                 token_info['lemma'] = self.add_lemma(token_info['lemma'], commit=False)
-    #                 token_info['pos'] = self.add_pos(token_info['pos'], commit=False)
-    #                 self.conn.execute('''
-    #                 UPDATE
-    #                     lemma_id = : lemma,
-    #                     pos_id = :pos,
-    #                 WHERE sent''')
+    def add_lemmas(self, lemmas: Iterable[Tuple[str, ]], commit: bool = True):
+        self.conn.executemany('''
+        INSERT or IGNORE INTO lemmas (name)
+        VALUES (?)''', lemmas)
+        if commit:
+            self.conn.commit()
 
+    def add_poses(self, poses: Iterable[Tuple[str, ]], commit: bool = True):
+        self.conn.executemany('''
+                INSERT or IGNORE INTO pos (name)
+                VALUES (?)''', poses)
+        if commit:
+            self.conn.commit()
 
+    def add_sentence_tokens(self, sentence: DatabaseSentence):
+        """
+        Add records about tokens
+        Args:
+            sentence: instance of DatabaseSentence class
+        """
+        lemmas = sentence.tokens_attr('lemma_', 'tuple')
+        poses = sentence.tokens_attr('lemma_', 'tuple')
+        tokens_info = sentence.token_info_generator()
+        with self.conn:
+            self.add_lemmas(lemmas, commit=False)
+            self.add_poses(poses, commit=False)
+            self.conn.executemany('''
+            INSERT INTO tokens (sent_id, token, whitespace, pos_in_text, char_start, char_end, pos_id, lemma_id)
+            VALUES (
+            (SELECT sents.id FROM sents 
+            LEFT JOIN texts 
+            ON texts.id = sents.text_id 
+            WHERE texts.filename = :filename 
+            AND sents.pos_in_text = :text_pos_in_text),
+            :token,
+            :whitespace, 
+            :pos_in_text,
+            :char_start, 
+            :char_end,
+            (SELECT id FROM poses WHERE name = :pos),
+            (SELECT id FROM lemmas WHERE name = :lemma))''', tokens_info)
+
+    def update_sentence_tokens_info(self, sentence: DatabaseSentence):
+        lemmas = sentence.tokens_attr("lemma_", "tuple")
+        poses = sentence.tokens_attr("lemma_", "tuple")
+        tokens_info = sentence.token_info_generator()
+        with self.conn:
+            self.add_lemmas(lemmas, commit=False)
+            self.add_poses(poses, commit=False)
+            self.conn.executemany('''
+            UPDATE tokens
+            SET 
+            lemma_id = (SELECT id FROM lemmas WHERE name = :lemma),
+            pos_id = (SELECT id FROM poses WHERE name = :pos)
+            WHERE sent_id = (SELECT sents.id FROM sents 
+            LEFT JOIN texts 
+            ON texts.id = sents.text_id 
+            WHERE texts.filename = :filename 
+            AND sents.pos_in_text = :text_pos_in_text)
+            AND pos_in_sent = :pos_in_sent''', tokens_info)
 
 
 if __name__ == '__main__':
