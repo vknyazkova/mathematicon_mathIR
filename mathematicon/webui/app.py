67d19c0116a4f988440cf2b2e7fa1600b0dcde11
Виктория Князькова <vknyazkova01@gmail.com>
2023-10-26 19:42:23 +0300

    Update app.py

--- mathematicon/webui/app.py
+++ mathematicon/webui/app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from pathlib import Path
 
-from secret import FLASK_SECRET_KEY
+from flask import Flask
+import spacy
+
+from .secret import FLASK_SECRET_KEY
+from .. import DB_PATH
+from ..backend.models.text_search import TextSearch
+from ..backend.models.database import WebDBHandler
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = FLASK_SECRET_KEY
+
+nlp = spacy.load('ru_core_news_sm')
+print(DB_PATH)
+db = WebDBHandler(DB_PATH)
 
 
 @app.route('/')
@@ -12,4 +23,6 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
+    text_search = TextSearch(db, nlp)
+    print(db.sent_context(1, 2))
     app.run()
