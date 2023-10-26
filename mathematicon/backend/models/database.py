ba9c033b38b53904dbbcec053fa138e39693a667
Виктория Князькова <vknyazkova01@gmail.com>
2023-10-26 19:41:47 +0300

    add WebDBHandler

--- mathematicon/backend/models/database.py
+++ mathematicon/backend/models/database.py
@@ -1,5 +1,6 @@
+import os
 import sqlite3
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, Union, List
 
 from .custom_dataclasses import DatabaseToken, DatabaseSentence, DatabaseText
 
@@ -7,8 +8,12 @@ from .custom_dataclasses import DatabaseToken, DatabaseSentence, DatabaseText
 class DBHandler:
     conn = None
 
-    def __init__(self, db_path):
-        self.conn = sqlite3.connect(db_path, check_same_thread=False)
+    def __init__(self,
+                 db_path: Union[str, os.PathLike]):
+        try:
+            self.conn = sqlite3.connect(db_path, check_same_thread=False)
+        except sqlite3.OperationalError:
+            raise sqlite3.OperationalError(db_path)
 
     def __del__(self):
         self.conn.close()
@@ -18,10 +23,15 @@ class DBHandler:
         fields = [column[0] for column in cursor.description]
         return {key: value for key, value in zip(fields, row)}
 
+    @staticmethod
+    def one_column_factory(cursor: sqlite3.Cursor, row):
+        return row[0]
+
 
 class UserDBHandler(DBHandler):
 
-    def get_user_by_uname(self, username):
+    def get_user_by_uname(self,
+                          username: str):
         cur = self.conn.execute('''
         SELECT * 
         FROM users
@@ -30,7 +40,10 @@ class UserDBHandler(DBHandler):
         cur.row_factory = self.dict_factory
         return cur.fetchone()
 
-    def add_user(self, username, password, salt, email):
+    def add_user(self,
+                 username: str,
+                 password: str,
+                 salt, email):
         self.conn.execute('''
         INSERT INTO users (username, password, salt, email)
         VALUES (?, ?, ?, ?)''', (username, password, salt, email))
@@ -39,25 +52,30 @@ class UserDBHandler(DBHandler):
 
 class TextDBHandler(DBHandler):
 
-    def add_math_branch(self, name: str, commit: bool = True):
+    def add_math_branch(self,
+                        name: str,
+                        commit: bool = True):
         self.conn.execute('''
         INSERT or IGNORE INTO math_branches (name)
         VALUES (?)''', (name, ))
         if commit:
             self.conn.commit()
 
-    def add_text_level(self, name, commit: bool = True):
+    def add_text_level(self,
+                       name: str,
+                       commit: bool = True):
         self.conn.execute('''
                     INSERT or IGNORE INTO text_difficulty (name)
                     VALUES (?)''', (name,))
         if commit:
             self.conn.commit()
 
-    def add_text(self, text: DatabaseText):
+    def add_text(self,
+                 text: DatabaseText):
         """
         Adds record about text to database or updates if filename exists
         Args:
-            text_info: instance of DatabaseText class
+            text: instance of DatabaseText class
         """
         text_info = text.dict_()
         try:
@@ -85,11 +103,12 @@ class TextDBHandler(DBHandler):
             self.conn.rollback()
             print(e)
 
-    def add_sentence(self, sentence: DatabaseSentence) -> int:
+    def add_sentence(self,
+                     sentence: DatabaseSentence) -> int:
         """
         Adds information about sentence
         Args:
-            sentence_info: instance of DatabaseSentence class
+            sentence: instance of DatabaseSentence class
         Returns:
 
         """
@@ -107,21 +126,26 @@ class TextDBHandler(DBHandler):
             self.conn.rollback()
             print(e)
 
-    def add_lemmas(self, lemmas: Iterable[Tuple[str, ]], commit: bool = True):
+    def add_lemmas(self,
+                   lemmas: Iterable[Tuple[str, ]],
+                   commit: bool = True):
         self.conn.executemany('''
         INSERT or IGNORE INTO lemmas (name)
         VALUES (?)''', lemmas)
         if commit:
             self.conn.commit()
 
-    def add_poses(self, poses: Iterable[Tuple[str, ]], commit: bool = True):
+    def add_poses(self,
+                  poses: Iterable[Tuple[str, ]],
+                  commit: bool = True):
         self.conn.executemany('''
                 INSERT or IGNORE INTO pos (name)
                 VALUES (?)''', poses)
         if commit:
             self.conn.commit()
 
-    def add_sentence_tokens(self, sentence: DatabaseSentence):
+    def add_sentence_tokens(self,
+                            sentence: DatabaseSentence):
         """
         Add records about tokens
         Args:
@@ -153,7 +177,8 @@ class TextDBHandler(DBHandler):
             self.conn.rollback()
             print(e)
 
-    def update_sentence_tokens_info(self, sentence: DatabaseSentence):
+    def update_sentence_tokens_info(self,
+                                    sentence: DatabaseSentence):
         lemmas = sentence.tokens_attr("lemma_", "tuple")
         poses = sentence.tokens_attr("tag_", "tuple")
         tokens_info = iter(sentence)
@@ -178,11 +203,62 @@ class TextDBHandler(DBHandler):
             print(e)
 
 
+class WebDBHandler(DBHandler):
+    def sents_with_query_words(self,
+                               lemmatized_query: Iterable[str]) -> Iterable[dict]:
+        pattern = '%' + '%'.join(lemmatized_query) + '%'
+        cur = self.conn.execute('''
+        SELECT sents.id, sents.lemmatized
+        FROM sents
+        WHERE sents.lemmatized LIKE ?''', (pattern,))
+        cur.row_factory = self.dict_factory
+        return cur.fetchall()
+
+    def sent_info(self,
+                  sent_id: int):
+        cur = self.conn.execute('''
+        SELECT sents.text_id, sents.pos_in_text, texts.youtube_link, sents.timecode
+        FROM sents
+        LEFT JOIN texts
+        ON sents.text_id = texts.id
+        WHERE sents.id = (?)         
+        ''', (sent_id,))
+        cur.row_factory = self.dict_factory
+        return cur.fetchone()
+
+    def sent_context(self,
+                     text_id: int,
+                     pos_in_text: int) -> list:
+        cur = self.conn.execute('''
+        SELECT sents.sent
+        FROM sents
+        WHERE text_id = :text_id
+        AND (pos_in_text = :pos_in_text - 1) OR (pos_in_text = :pos_in_text + 1)
+        ORDER BY sents.pos_in_text
+        ''', {'text_id': text_id, 'pos_in_text': pos_in_text})
+        cur.row_factory = self.one_column_factory
+        return cur.fetchall()
+
+    def sent_token_info(self,
+                        sent_id: int):
+        cur = self.conn.execute('''
+        SELECT tokens.token, tokens.whitespace, pos.name AS 'pos', lemmas.name AS 'lemma'
+        FROM tokens
+        LEFT JOIN lemmas
+        ON lemmas.id = tokens.lemma_id
+        LEFT JOIN pos
+        ON pos.id = tokens.pos_id
+        WHERE tokens.sent_id = (?)
+        ORDER BY tokens.pos_in_sent
+        ''', (sent_id,))
+
+
+
 if __name__ == '__main__':
     from mathematicon.config import DATA_PATH
     from pathlib import Path
 
     db_path = Path(DATA_PATH, 'mathematicon.db')
 
-    db = UserDBHandler(db_path)
-    print(db.get_user_by_uname('vknyazkova'))
+    db = WebDBHandler(db_path)
+    print(db.sents_with_query_words(['бэ', 'степень']))
