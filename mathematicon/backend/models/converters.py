291f9f559df41d5679346c1754cd3339c3d1c37b
Виктория Князькова <vknyazkova01@gmail.com>
2023-11-10 19:14:16 +0300

    added morphology annotation to db

--- mathematicon/backend/models/converters.py
+++ mathematicon/backend/models/converters.py
@@ -92,3 +92,27 @@ def update_ud_annot(conllu_file: Union[str, os.PathLike],
     conllu_doc = conllu_nlp.parse_conll_file_as_spacy(conllu_file)
     for sent in DatabaseText(conllu_doc, filename=filename):
         db.update_sentence_tokens_info(sent)
+
+#
+# if __name__ == '__main__':
+#     files = input('filepaths: ').split(' ')
+#     yaml_converter = YamlConverter(files)
+#
+#     from mathematicon import DB_PATH
+#     from mathematicon.backend.models.mathematicon_morph_parser import MorphologyCorrectionHandler
+#     from spacy.language import Language
+#     from spacy_conll import init_parser
+#
+#     db = TextDBHandler(DB_PATH)
+#     @Language.factory(
+#         "morphology_corrector",
+#         assigns=["token.lemma", "token.tag"],
+#         requires=["token.pos"],
+#         default_config={"mode": "ptcp+conv"},
+#     )
+#     def morphology_corrector(nlp, name, mode):
+#         return MorphologyCorrectionHandler(mode)
+#     nlp = init_parser("ru_core_news_sm", 'spacy', include_headers=True, exclude_spacy_components=['ner'])
+#
+#     print(yaml_converter.yaml_contents.values())
+#     yaml_converter.to_database(nlp, db)
