7045b4a49066c6db19c2e8efd1601a901beb082d
Виктория Князькова <vknyazkova01@gmail.com>
2024-05-18 22:07:36 +0300

    add functions for testing

--- mathematicon/backend/repositories/lecture_repo.py
+++ mathematicon/backend/repositories/lecture_repo.py
@@ -1,18 +1,75 @@
 import sqlite3
 from contextlib import closing
+from typing import Optional
 
 from ..model import MathLecture
 
 
 class LectureRepository:
-    def __init__(self, dp_path: str):
-        self._dp_path = dp_path
+    def __init__(self,
+                 dp_path: str,
+                 db_conn: Optional[sqlite3.Connection] = None):
+        self.dp_path = dp_path
+        self.conn = db_conn
 
-    def get_math_branch_id(self,
-                           conn: sqlite3.Connection,
-                           name: str) -> int:
-        cur = conn.cursor()
-        cur.execute('''SELECT name
+    def connect(self):
+        if self.conn is None:
+            self.conn = sqlite3.connect(self.dp_path)
+
+    def close(self):
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def create_tables(self):
+        self.connect()
+        cursor = self.conn.cursor()
+
+        # Create math_branches table
+        cursor.execute('''
+            CREATE TABLE math_branches (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE
+            )
+            ''')
+
+        # Create text_difficulty table
+        cursor.execute('''
+            CREATE TABLE text_difficulty (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE
+            )
+            ''')
+
+        # Create texts table
+        cursor.execute('''
+            CREATE TABLE texts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT,
+                filename TEXT UNIQUE,
+                youtube_link TEXT UNIQUE,
+                timecode_start TEXT,
+                timecode_end TEXT,
+                math_branch_id INTEGER,
+                level_id INTEGER,
+                FOREIGN KEY (math_branch_id) REFERENCES math_branches (id) ON DELETE SET NULL ON UPDATE CASCADE,
+                FOREIGN KEY (level_id) REFERENCES text_difficulty (id) ON DELETE SET NULL ON UPDATE CASCADE
+            )
+            ''')
+
+        self.conn.commit()
+
+    def _get_math_branch_id(self,
+                            name: str) -> int:
+        cur = self.conn.cursor()
+        cur.execute('''SELECT id
         FROM math_branches
         WHERE name = (?)''', (name, ))
         id_ = cur.fetchone()
@@ -24,11 +81,10 @@ class LectureRepository:
             id_ = cur.fetchone()
         return id_[0]
 
-    def get_text_difficulty_id(self,
-                               conn: sqlite3.Connection,
-                               name: str) -> int:
-        cur = conn.cursor()
-        cur.execute('''SELECT name
+    def _get_text_difficulty_id(self,
+                                name: str) -> int:
+        cur = self.conn.cursor()
+        cur.execute('''SELECT id
                 FROM text_difficulty
                 WHERE name = (?)''', (name,))
         id_ = cur.fetchone()
@@ -40,15 +96,16 @@ class LectureRepository:
             id_ = cur.fetchone()
         return id_[0]
 
-    def add_lecture(self, lecture: MathLecture) -> MathLecture:
-        with closing(sqlite3.connect(self._dp_path)) as conn:
-            cur = conn.cursor()
-            math_branch_id = self.get_math_branch_id(conn, lecture.math_branch)
-            text_difficulty_id = self.get_text_difficulty_id(conn, lecture.difficulty_level)
-            cur.execute('''
-            INSERT INTO texts (title, filename, youtube_link, timecode_start, timecode_end, math_branch_id, level_id)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-            RETURNING id''', (
+    def add_lecture(self,
+                    lecture: MathLecture) -> MathLecture:
+        self.connect()
+        with self.conn:
+            math_branch_id = self._get_math_branch_id(lecture.math_branch)
+            text_difficulty_id = self._get_text_difficulty_id(lecture.difficulty_level)
+            cur = self.conn.execute('''
+                                INSERT INTO texts (title, filename, youtube_link, timecode_start, timecode_end, math_branch_id, level_id)
+                                VALUES (?, ?, ?, ?, ?, ?, ?)
+                                RETURNING id''', (
                 lecture.title,
                 lecture.filename,
                 lecture.youtube_link,
@@ -57,5 +114,44 @@ class LectureRepository:
                 math_branch_id,
                 text_difficulty_id
             ))
-            lecture.lecture_id = cur.fetchone()
+            lecture.lecture_id = cur.fetchone()[0]
         return lecture
+
+
+if __name__ == '__main__':
+    db_path = ':memory:'
+    conn = sqlite3.connect(db_path)
+    lecture_repo = LectureRepository(db_path, conn)
+
+    lecture_repo.create_tables()
+    cur = conn.execute('''SELECT name FROM sqlite_master WHERE type='table';''')
+    print(cur.fetchall())
+
+    lecture1 = MathLecture(
+        title="Sample Lecture",
+        filename="sample.mp4",
+        youtube_link="https://youtube.com/sample",
+        timecode_start="00:00:00",
+        timecode_end="01:00:00",
+        math_branch="Algebra",
+        difficulty_level="Intermediate"
+    )
+
+    lecture2 = MathLecture(
+        title="Sample Lecture2",
+        filename="sample2.mp4",
+        youtube_link="https://youtube.com/sample2",
+        timecode_start="00:00:00",
+        timecode_end="01:10:00",
+        math_branch="Geometry",
+        difficulty_level="Intermediate"
+    )
+    try:
+        lecture1 = lecture_repo.add_lecture(lecture1)
+        print(lecture1)
+        lecture2 = lecture_repo.add_lecture(lecture2)
+        print(lecture2)
+    except Exception as e:
+        cur = conn.execute('''SELECT * FROM math_branches''')
+        print(cur.fetchall())
+    conn.close()
--- mathematicon/backend/repositories/transcript_repo.py
+++ mathematicon/backend/repositories/transcript_repo.py
@@ -6,13 +6,125 @@ from ..model import Sentence, Token
 
 
 class TranscriptRepository:
-    def __init__(self, db_path):
+    def __init__(self,
+                 db_path: str,
+                 db_conn: Optional[sqlite3.Connection] = None):
         self.db_path = db_path
+        self.conn = db_conn
+
+    def connect(self):
+        if self.conn is None:
+            self.conn = sqlite3.connect(self.db_path)
+
+    def close(self):
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def create_tables(self,
+                      foreign_key_tables: bool = False):
+        self.connect()
+        cursor = self.conn.cursor()
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS texts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            filename TEXT,
+            youtube_link TEXT,
+            timecode_start TEXT,
+            timecode_end TEXT,
+            math_branch_id INTEGER,
+            level_id INTEGER
+        )''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS sents (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text_id INTEGER,
+            sent TEXT,
+            lemmatized TEXT,
+            pos_in_text INTEGER,
+            timecode TEXT,
+            FOREIGN KEY (text_id) REFERENCES texts (id)
+                ON DELETE CASCADE ON UPDATE CASCADE
+        )''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS lemmas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE
+        )''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS pos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE,
+            descr_rus TEXT,
+            descr_eng TEXT,
+            examples TEXT,
+            UD_link TEXT UNIQUE
+        )''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tokens (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sent_id INTEGER,
+            pos_in_sent INTEGER,
+            token TEXT,
+            whitespace BOOLEAN,
+            lemma_id INTEGER,
+            pos_id INTEGER,
+            char_start INTEGER,
+            char_end INTEGER,
+            FOREIGN KEY (sent_id) REFERENCES sents (id)
+                ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (lemma_id) REFERENCES lemmas (id)
+                ON UPDATE CASCADE ON DELETE SET NULL,
+            FOREIGN KEY (pos_id) REFERENCES pos (id)
+                ON UPDATE CASCADE ON DELETE SET NULL
+        )''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS morph_categories (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE
+        )''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS morph_values (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE
+        )''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS morph_features (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            token_id INTEGER,
+            category_id INTEGER,
+            value_id INTEGER,
+            FOREIGN KEY (token_id) REFERENCES tokens (id)
+                ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (category_id) REFERENCES morph_categories (id)
+                ON DELETE SET NULL ON UPDATE CASCADE,
+            FOREIGN KEY (value_id) REFERENCES morph_values (id)
+                ON DELETE SET NULL ON UPDATE CASCADE,
+            UNIQUE (token_id, category_id)
+        )''')
+
+        self.conn.commit()
+
 
     def _add_sentence(self,
-                      conn: sqlite3.Connection,
                       sentence: Sentence):
-        cur = conn.execute(
+        cur = self.conn.execute(
             '''
             INSERT INTO sents (text_id, sent, lemmatized, pos_in_text)
             VALUES (?, ?, ?, ?)
@@ -26,9 +138,8 @@ class TranscriptRepository:
         return cur.fetchone()[0]
 
     def _get_lemma_id(self,
-                      conn: sqlite3.Connection,
                       lemma: str) -> int:
-        cur = conn.execute('''SELECT id
+        cur = self.conn.execute('''SELECT id
         FROM lemmas
         WHERE name = (?)''', (lemma, ))
         id_ = cur.fetchone()
@@ -41,9 +152,8 @@ class TranscriptRepository:
         return id_[0]
 
     def _get_pos_id(self,
-                    conn: sqlite3.Connection,
                     pos: str) -> int:
-        cur = conn.execute('''SELECT id
+        cur = self.conn.execute('''SELECT id
                 FROM pos
                 WHERE name = (?)''', (pos,))
         id_ = cur.fetchone()
@@ -56,9 +166,8 @@ class TranscriptRepository:
         return id_[0]
 
     def _get_morph_category_id(self,
-                               conn: sqlite3.Connection,
                                category: str) -> int:
-        cur = conn.execute('''SELECT id
+        cur = self.conn.execute('''SELECT id
                         FROM morph_categories
                         WHERE name = (?)''', (category,))
         id_ = cur.fetchone()
@@ -71,9 +180,8 @@ class TranscriptRepository:
         return id_[0]
 
     def _get_morph_value_id(self,
-                            conn: sqlite3.Connection,
                             value: str) -> int:
-        cur = conn.execute('''SELECT id
+        cur = self.conn.execute('''SELECT id
                         FROM morph_values
                         WHERE name = (?)''', (value,))
         id_ = cur.fetchone()
@@ -86,23 +194,21 @@ class TranscriptRepository:
         return id_[0]
 
     def _add_morph_info(self,
-                        conn: sqlite3.Connection,
                         token_id: int,
                         morph_info: str):
         for morph in morph_info.split('|'):
             category, value = morph.split('=')
-            category_id = self._get_morph_category_id(conn, category)
-            value_id = self._get_morph_value_id(conn, value)
-            conn.execute('''
+            category_id = self._get_morph_category_id(category)
+            value_id = self._get_morph_value_id(value)
+            self.conn.execute('''
             INSERT INTO morph_features (token_id, category_id, value_id) 
             VALUES (?, ?, ?)''', (token_id, category_id, value_id))
 
     def _add_token(self,
-                   conn: sqlite3.Connection,
                    token: Token):
-        lemma_id = self._get_lemma_id(conn, token.lemma)
-        pos_id = self._get_pos_id(conn, token.pos_tag)
-        cur = conn.execute('''
+        lemma_id = self._get_lemma_id(token.lemma)
+        pos_id = self._get_pos_id(token.pos_tag)
+        cur = self.conn.execute('''
         INSERT INTO tokens (sent_id, pos_in_sent, token, whitespace, char_start, char_end, lemma_id, pos_id) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         RETURNING id''', (
@@ -116,16 +222,16 @@ class TranscriptRepository:
             pos_id
         ))
         token_id = cur.fetchone()[0]
-        self._add_morph_info(conn, token_id, token.morph_annotation)
+        self._add_morph_info(token_id, token.morph_annotation)
 
     def add_transcript(self, sentences: List[Sentence]):
-        with closing(sqlite3.connect(self.db_path)) as conn:
+        self.connect()
+        with self.conn:
             for sentence in sentences:
-                sent_id = self._add_sentence(conn, sentence)
+                sent_id = self._add_sentence(sentence)
                 for token in sentence.tokens:
                     token.sentence_id = sent_id
-                    self._add_token(conn, token)
-        conn.close()
+                    self._add_token(token)
 
     def search_lemmatized(self, lemmatized_query: List[str]) -> List[Sentence]:
         ...
@@ -160,3 +266,13 @@ class TranscriptRepository:
 
     def get_sentence_yb_link(self, sentence: Sentence) -> str:
         ...
+
+
+if __name__ == '__main__':
+    db_path = ':memory:'
+    conn = sqlite3.connect(db_path)
+    lecture_repo = TranscriptRepository(db_path, conn)
+
+    lecture_repo.create_tables()
+    cur = conn.execute('''SELECT name FROM sqlite_master WHERE type='table';''')
+    print(cur.fetchall())
